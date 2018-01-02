70c7eac93fe604e850a8b75cade2fa615e760bfb
raresica1234 <colorlord98@gmail.com>
2018-01-02 19:52:22 +0200

    Lots of changes to ui, moved output objects and intermidiates to separate directory

--- Exporter/Exporter.cpp
+++ Exporter/Exporter.cpp
@@ -3,19 +3,88 @@
 Exporter::Exporter(QWidget *parent)
 	: QMainWindow(parent) {
 	ui.setupUi(this);
-	m_Button = ui.pushButton;
-	m_ProgressBar = ui.progressBar;
-	m_ProgressBar->setValue(0);
+	m_PathButton = ui.pushButton;
+
+	m_ResourceBar = ui.resourceBar;
+	m_ResourceBar->setValue(0);
+
+	m_ResourcesBar = ui.resourcesBar;
+	m_ResourcesBar->setValue(0);
+
+	m_ProgressAction = ui.progressAction;
+
 	m_Paths = ui.files;
+	m_Paths->setSelectionMode(QAbstractItemView::ExtendedSelection);
+
 	m_ImageIcon = new QIcon("Resources/image.png");
 	m_ModelIcon = new QIcon("Resources/model.png");
-	connect(m_Button, SIGNAL(released()), this, SLOT(SelectPathButton()));
+
+	m_ExportLocation = ui.exportLocation;
+	m_ExportLocation->setText("Export Location: " + QDir::currentPath());
+	m_ExportButton = ui.changeLocation;
+
+	m_DeleteMenu = new QMenu("Action");
+	m_DeleteMenu->addAction("Convert single", this, SLOT(ConvertSingle()));
+	m_DeleteMenu->addAction("Delete entry", this, SLOT(DeleteItem()));
+
+	m_Paths->setContextMenuPolicy(Qt::CustomContextMenu);
+
+	connect(m_Paths, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(PathsRightClick(QPoint)));
+	connect(m_PathButton, SIGNAL(released()), this, SLOT(SelectPathButton()));
+	connect(m_ExportButton, SIGNAL(released()), this, SLOT(SelectExportLocation()));
 }
 
+Exporter::~Exporter() {
+	delete m_ImageIcon, m_ModelIcon;
+}
 
 void Exporter::SelectPathButton() {
-	QString fileName = QFileDialog::getOpenFileName(this, "Open Image", QDir::currentPath(), tr("Image files (*.png *.jpg *.bmp *.tga *.jpeg);;Model files (*.obj)"));
-	QListWidgetItem* item = new QListWidgetItem(fileName);
-	item->setIcon(*m_ImageIcon);
-	m_Paths->addItem(item);
+	QFileDialog dialog;
+	dialog.setAcceptMode(QFileDialog::AcceptOpen);
+	dialog.setNameFilter("Image files (*.png *.jpg *.bmp *.tga *.jpeg);;Model files (*.obj)");
+	dialog.setFileMode(QFileDialog::ExistingFiles);
+	dialog.setDirectory(QDir::current());
+
+	if (dialog.exec() == QDialog::Accepted) {
+		QStringList fileNames = dialog.selectedFiles();
+		QString selectedFilter = dialog.selectedNameFilter();
+		if (selectedFilter.split(" ")[0] == "Image") {
+			for (int i = 0; i < fileNames.size(); i++) {
+				QListWidgetItem* item = new QListWidgetItem(*m_ImageIcon, fileNames[i]);
+				m_Paths->addItem(item);
+			}
+		}
+		else if (selectedFilter.split(" ")[0] == "Model") {
+			for (int i = 0; i < fileNames.size(); i++) {
+				QListWidgetItem* item = new QListWidgetItem(*m_ModelIcon, fileNames[i]);
+				m_Paths->addItem(item);
+			}
+		}
+	}
+}
+
+void Exporter::PathsRightClick(QPoint location) {
+	m_DeleteMenu->exec(m_Paths->mapToGlobal(location));
+}
+
+void Exporter::ConvertSingle() {
+
+}
+
+void Exporter::DeleteItem() {
+	auto items = m_Paths->selectedItems();
+	for (QListWidgetItem* item : items) {
+		delete item;
+	}
+}
+
+void Exporter::SelectExportLocation() {
+	QFileDialog dialog;
+	dialog.setAcceptMode(QFileDialog::AcceptOpen);
+	dialog.setFileMode(QFileDialog::DirectoryOnly);
+	dialog.setDirectory(QDir::current());
+	if (dialog.exec() == QDialog::Accepted) {
+		QString location = dialog.selectedFiles()[0];
+		m_ExportLocation->setText("Export Location: " + location);
+	}
 }
--- Exporter/Exporter.h
+++ Exporter/Exporter.h
@@ -2,6 +2,8 @@
 
 #include <QtWidgets/QMainWindow>
 #include <QFileDialog>
+#include <QMouseEvent>
+
 #include "ui_Exporter.h"
 
 class Exporter : public QMainWindow
@@ -10,17 +12,32 @@ class Exporter : public QMainWindow
 
 private:
 	Ui::ExporterClass ui;
-	QPushButton* m_Button;
-	QProgressBar* m_ProgressBar;
+	QPushButton* m_PathButton;
+	QPushButton* m_ExportButton;
+
+	QProgressBar* m_ResourceBar;
+	QProgressBar* m_ResourcesBar;
+
 	QListWidget* m_Paths;
+
+	QLabel* m_ProgressAction;
+	QLabel* m_ExportLocation;
 
 	QIcon* m_ImageIcon;
 	QIcon* m_ModelIcon;
 
+	QMenu* m_DeleteMenu;
+
 public:
 	Exporter(QWidget *parent = Q_NULLPTR);
+	~Exporter();
 
 private slots:
 	void SelectPathButton();
+	void PathsRightClick(QPoint location);
 
+	void ConvertSingle();
+	void DeleteItem();
+
+	void SelectExportLocation();
 };
